65c742dcdda879755b611c6b893e91dcf636d881
Rhodri James <rhodri@kynesim.co.uk>
2017-03-09 17:05:50 +0000

    Extend NotStandalone tests to also exclude external entity handlers

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -1409,6 +1409,12 @@ START_TEST(test_not_standalone_handler_reject)
     XML_SetNotStandaloneHandler(parser, reject_not_standalone_handler);
     expect_failure(text, XML_ERROR_NOT_STANDALONE,
                    "NotStandalone handler failed to reject");
+
+    /* Try again but without external entity handling */
+    XML_ParserReset(parser, NULL);
+    XML_SetNotStandaloneHandler(parser, reject_not_standalone_handler);
+    expect_failure(text, XML_ERROR_NOT_STANDALONE,
+                   "NotStandalone handler failed to reject");
 }
 END_TEST
 
@@ -1435,6 +1441,11 @@ START_TEST(test_not_standalone_handler_accept)
     XML_SetExternalEntityRefHandler(parser, external_entity_loader);
     XML_SetNotStandaloneHandler(parser, accept_not_standalone_handler);
     run_ext_character_check(text, &test_data, "");
+
+    /* Repeat wtihout the external entity handler */
+    XML_ParserReset(parser, NULL);
+    XML_SetNotStandaloneHandler(parser, accept_not_standalone_handler);
+    run_character_check(text, "");
 }
 END_TEST
 
