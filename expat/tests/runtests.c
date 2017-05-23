46ec56ff78c6010ed84856d093682628aa0357e4
Rhodri James <rhodri@kynesim.co.uk>
2017-05-23 16:15:25 +0100

    Add wrapper macro to give string literals the right type for XML_Char *

--- expat/tests/runtests.c
+++ expat/tests/runtests.c
@@ -43,6 +43,8 @@
 #define XML_CHAR_strlen(s) wcslen(s)
 #define XML_CHAR_strcmp(s, t) wcscmp((s), (t))
 #define XML_CHAR_strncmp(s, t, n) wcsncmp((s), (t), (n))
+#define XML_CHAR_CONST(s) _XML_CHAR_CONST(s)
+#define _XML_CHAR_CONST(s) L ## s
 #else
 #ifdef XML_UNICODE
 #error "No support for UTF-16 without wchar_t in tests"
@@ -52,6 +54,7 @@
 #define XML_CHAR_strlen(s) strlen(s)
 #define XML_CHAR_strcmp(s, t) strcmp((s), (t))
 #define XML_CHAR_strncmp(s, t, n) strncmp((s), (t), (n))
+#define XML_CHAR_CONST(s) s
 #endif
 #endif
 
